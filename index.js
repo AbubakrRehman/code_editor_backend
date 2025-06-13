6224d9e00a62a9b8359680c644e19fea49158b0c
AbubakrRehman <abubakr.rehman91@outlook.com>
2025-06-13 14:09:41 +0530

    add cors origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = 3000;
 
 // Enable CORS
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_DOMAIN, // Set your allowed origin here
     credentials: true
 }));
 
